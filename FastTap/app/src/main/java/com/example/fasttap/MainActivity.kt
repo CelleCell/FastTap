c5ad4e8c440f6175e024ac338ba57bea2e7d1184
Morrison <lsm2@njit.edu>
2024-01-25 23:23:21 -0500

    Fast Tap Project

---
+++ FastTap/app/src/main/java/com/example/fasttap/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.fasttap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.*
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    var counter = 0;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button);
+        val textView = findViewById<TextView>(R.id.textView)
+        val upgrade = findViewById<Button>(R.id.upgradeBtn);
+        var counteramount = 1;
+        val upgradeButton = findViewById<Button>(R.id.upgradeBtn)
+
+        button.setOnClickListener {
+            // Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+
+            if (counter >= 100) {
+
+                // Show upgrade button and set onClickListener
+                upgradeButton.visibility = View.VISIBLE
+                upgradeButton.setOnClickListener {
+                    button.text = "Add 2"
+
+                    // Update original button to add 2 instead of `
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+
+                    // Hide upgrade button again
+                    upgradeButton.visibility = View.INVISIBLE
+                }
+            }
+        }
+    }
+}
